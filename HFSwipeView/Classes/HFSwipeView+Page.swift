5fd85069d5963550a0fad0739c48a61dce68259c
kt <thiering@fireflow.de>
2017-11-06 09:21:09 +0100

    reverted

--- HFSwipeView/Classes/HFSwipeView+Page.swift
+++ HFSwipeView/Classes/HFSwipeView+Page.swift
@@ -21,16 +21,8 @@ extension HFSwipeView {
         let realIndex = IndexPath(item: realPage, section: 0)
         
         if autoAlignEnabled {
-
-            //bugfix for circulating swipeviews where the offset is too high
             let offset = centeredOffsetForIndex(realIndex)
-            if circulating {
-                let current = collectionView.indexPathsForVisibleItems.first ?? realIndex
-                let indexPath = IndexPath(row: current.row + 1, section: 0)
-                collectionView.scrollToItem(at: indexPath, at: .left, animated: true)
-            } else {
-                collectionView.setContentOffset(offset, animated: animated)
-            }
+            collectionView.setContentOffset(offset, animated: animated)
         }
         
         if !circulating {
